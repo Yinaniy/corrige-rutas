3b15ce74f24097d4d3604cc51d57d528053a2f1f
Yinaniy <yinnahm@gmail.com>
2020-08-04 11:10:36 -0500

    Es del proyecto de ayer

---
+++ R/01_rutas_absolutas.R
@@ -0,0 +1,6 @@
+# Arregla la ruta y carga los datos
+datos <- read.csv("C:/Users/ginaf/corrige-rutas-master/datos/2019_rama-pm25.csv")
+datos <- read.csv("~/corrige-rutas-master/datos/2019_rama-pm25.csv")
+
+# Ve los datos
+View(datos)
